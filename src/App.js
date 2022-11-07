d414df92a197e0fb73c12e73ce34e841d6c5ae32
benaber1990 <104931001+benaber1990@users.noreply.github.com>
2022-11-07 10:50:23 +0000

    deploy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import MainThisWeek from './components/MainThisWeek';
 
 const App = () => {
   return <div>
-    <p>Hello World!</p>
+  <div style={{ display: 'flex', flexDirection: 'row' }}>
     <MainThisWeek title="What's on This Week"/>
     <MainThisWeek title="Latest Lesson Link"/>
     <MainThisWeek title="Student Work Showcase"/>
-    
+    </div>
   </div>
 }
 
