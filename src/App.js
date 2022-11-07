86e543919a567f92cbdd8eb6b86db284bd930543
benaber1990 <104931001+benaber1990@users.noreply.github.com>
2022-11-07 10:46:58 +0000

    hello

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import MainThisWeek from './components/MainThisWeek';
+
 
 const App = () => {
   return <div>
     <p>Hello World!</p>
+    <MainThisWeek title="What's on This Week"/>
   </div>
 }
 
