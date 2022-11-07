fffcf19afdc6814ba62e523360b6848eb5f8407f
benaber1990 <104931001+benaber1990@users.noreply.github.com>
2022-11-07 10:48:24 +0000

    deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ const App = () => {
   return <div>
     <p>Hello World!</p>
     <MainThisWeek title="What's on This Week"/>
+    <MainThisWeek title="Latest Lesson Link"/>
+    <MainThisWeek title="Student Work Showcase"/>
+    
   </div>
 }
 
