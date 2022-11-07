e62b46c1fbe6034f37f156b8d81ec89733966f62
benaber1990 <104931001+benaber1990@users.noreply.github.com>
2022-11-07 10:34:19 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css";
+import LogInModal from "./components/LogInModal";
+import MainThisWeek from "./components/MainThisWeek";
+import UploadFile from "./components/UploadFile";
 
 function App() {
-  return <div>
-    <p>Hello World</p>
-  </div>
+  const [logIn, setLogIn] = useState(true);
+
+  const LogInHandler = () => {
+    setLogIn((prevState) => !prevState);
+  };
+
+  return (
+    <div>
+      {logIn ? (
+        <LogInModal onClick={LogInHandler} />
+      ) : (
+        <div>
+          <h2 style={{ textAlign: "center" }}>Tutor Portal</h2>
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+              justifyContent: "center",
+            }}
+          >
+            <button onClick={LogInHandler}>Log Out</button>
+          </div>
+          <div className="App">
+            <MainThisWeek
+              title="What's on This Week"
+              // kyesTag="34534"
+              // description="Your info text can go here"
+            />
+            <MainThisWeek
+              title="Latest Lessons"
+              // description="Put you info text here"
+            />
+            <MainThisWeek
+              title="Student Showcase"
+              // description="Your details can go here"
+            />
+          </div>
+          <div className="UploadFileStyle">
+            <UploadFile />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App; 
+export default App;
